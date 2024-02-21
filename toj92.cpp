cd063957142cf883a16db22d646f447b414c28de
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-02-21 15:52:12 +0800

    Add files via upload

---
+++ toj92.cpp
@@ -0,0 +1,11 @@
+//https://toj.tfcis.org/oj/pro/92/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string a,b,c;
+    cin>>a>>b>>c;
+    cout<<"Hello, "<<a<<", "<<b<<", and "<<c<<"!\n";
+    return 0;
+}
