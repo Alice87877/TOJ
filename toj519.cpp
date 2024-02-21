68a2de710aaf4c338db2a7c45477278f44bb686a
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-02-21 15:55:04 +0800

    Add files via upload

---
+++ toj519.cpp
@@ -0,0 +1,11 @@
+//https://toj.tfcis.org/oj/pro/519/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<"Do you want to say "<<a<<" and "<<b<<" ??";
+    return 0;
+}
