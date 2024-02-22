91faac7672628056ada12cec48b1dd60eb4237e4
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-02-22 22:10:54 +0800

    Add files via upload

---
+++ toj522.cpp
@@ -0,0 +1,10 @@
+//https://toj.tfcis.org/oj/pro/522/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x; cin>>x;
+    cout<<x*x%10;
+    return 0;
+}
---
+++ toj95.cpp
@@ -0,0 +1,28 @@
+//https://toj.tfcis.org/oj/pro/95/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b; cin>>a>>b;
+    if(a==1){
+        if(b>=8){
+            cout<<"PASS!";
+        }else{
+            cout<<"FAIL! You see see you!";
+        }
+    }else if(a==2){
+        if(b>=9){
+            cout<<"PASS!";
+        }else{
+            cout<<"FAIL! You see see you!";
+        }
+    }else{
+        if(b>=10){
+            cout<<"PASS!";
+        }else{
+            cout<<"FAIL! You see see you!";
+        }
+    }
+    return 0;
+}
