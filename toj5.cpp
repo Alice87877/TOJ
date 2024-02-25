67fd6295b65603545cc6832aae7de6f6e84c71bf
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-02-25 17:03:28 +0800

    Add files via upload

---
+++ toj5.cpp
@@ -0,0 +1,11 @@
+//https://toj.tfcis.org/oj/pro/5/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string name;
+    getline(cin,name);
+    cout<<"Hello ,"<<name<<" !";
+    return 0;
+}
