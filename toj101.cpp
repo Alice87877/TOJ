cc2f84bf34eb251464aa54b319c3ddf503da2248
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-02-25 17:04:24 +0800

    Add files via upload

---
+++ toj101.cpp
@@ -0,0 +1,13 @@
+//https://toj.tfcis.org/oj/pro/101/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    char a='A'-1;
+    cin>>n;
+    a+=n;
+    cout<<a;
+    return 0;
+}
