6c96ccf3864818ab8131bf9a439294c3555d4487
ㄒㄒ芝 <99130819+Alice87877@users.noreply.github.com>
2024-02-27 21:41:05 +0800

    Add files via upload

---
+++ toj119.cpp
@@ -0,0 +1,28 @@
+//https://toj.tfcis.org/oj/pro/119/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int s[200001],n,i=0; cin>>n;
+    bool flag=true;
+    while(n--){
+        cin>>s[i];
+        i++;
+    }
+    cin>>n;
+    while(n--){
+        int a,b;
+        cin>>a>>b;
+        if(abs(b-a)<=8){
+            swap(s[b-1],s[a-1]);
+        }else{
+            flag=false;
+            break;
+        }
+    }
+    if(flag) cout<<"SORTED!\n";
+    else cout<<"I QUIT!\n";
+    for(int j=0;j<i-1;j++) cout<<s[j]<<" ";
+    cout<<s[i-1]<<"\n";
+    return 0;
+}
